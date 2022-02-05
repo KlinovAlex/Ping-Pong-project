e803c1b6c0a3d39b1bb81e15dae4dc2b6aa49675
unnamed <betacind@ga-j1.msk.21-school.ru>
2022-02-05 17:17:08 +0300

    Make get_move function

--- src/pong.c
+++ src/pong.c
@@ -3,10 +3,10 @@
 # include <stdio.h>
 # include <math.h>
 
-# define KEY_P1_UP a
-# define KEY_P1_DOWN z
-# define KEY_P2_UP k
-# define KEY_P2_DOWN m
+# define KEY_P1_UP 97
+# define KEY_P1_DOWN 122
+# define KEY_P2_UP 107
+# define KEY_P2_DOWN 109
 # define RACKET_SIZE 3
 # define BALL_SIZE 1
 # define FIELD_WIDTH 80
@@ -14,13 +14,18 @@
 # define FIELD_WIDTH_START 0
 # define FIELD_HEIGHT_START 0
 # define WIN_SCORE 21
+# define PLAYER_1_MOVE 1
+# define PLAYER_2_MOVE 0
 
 void game_end();
 void clear_screen();
 void draw_pole(int player1_racket_x, int player1_racket_y, int player2_racket_x,
             int player2_racket_y, int ball_cur_pos_x, int ball_cur_pos_y);
-//Structure of ball
 
+int ball_position();
+int get_vertical_center_position();
+int player_turn(int ball_cur_pos_x);
+void get_move(int ball_cur_pos_x);
 
 
 //Structure of ball
@@ -43,11 +48,13 @@ int player2_score;
 
 
 int main() {
-    if (player1_score < 21 || player2_score < 21) {
-		draw_pole (0, 0, 0, 0, 20, 20);
-    } else {
-		game_end();
-	}
+	while (player1_score < 21 || player2_score < 21){
+		// draw_pole (0, 0, 0, 0, 20, 20);
+		get_move(60);
+		// printf("%d\n", player2_racket_y);
+		// printf("%d\n", player1_racket_y);
+    }
+	game_end();
     return 0;
 }
 
@@ -60,6 +67,68 @@ void game_end() {
 void clear_screen() {
 // Function nave to clean screen after move
 }
+
+
+void put_ball_position (int x, int y) {
+	// Put ball position
+	ball_cur_pos_x = x;
+	ball_cur_pos_y = y;
+}
+
+int get_vertical_center_position() {
+	return FIELD_WIDTH/2;
+}
+
+int player_turn(int ball_cur_pos_x) {
+	// Function defines players turn.
+	// If function returns 1 - Player 1 turned
+	// If function returns 0 - Player 2 turned
+	if (ball_cur_pos_x <= get_vertical_center_position()) {
+		return PLAYER_1_MOVE;
+	} else {
+		return PLAYER_2_MOVE;
+	}
+}
+
+
+void get_move(int ball_cur_pos_x){
+	// Function waiting players move define who's turn and write players y position
+	char key_pressed;
+	key_pressed = getchar();
+
+	if(player_turn(ball_cur_pos_x) == PLAYER_1_MOVE) {
+		if (key_pressed == KEY_P1_UP) {
+			player1_racket_y--;
+			if (player1_racket_y < 0) {
+				player1_racket_y=0;
+			}
+		}
+		if (key_pressed == KEY_P1_DOWN) {
+			player1_racket_y++;
+			if (player1_racket_y+RACKET_SIZE >= FIELD_HEIGHT) {
+				player1_racket_y-=RACKET_SIZE-1;
+			}
+		}
+	}
+	if(player_turn(ball_cur_pos_x) == PLAYER_2_MOVE) {
+		if (key_pressed == KEY_P2_UP) {
+			player2_racket_y--;
+			if (player2_racket_y < 0) {
+				player2_racket_y=0;
+			}
+		}
+		if (key_pressed == KEY_P2_DOWN) {
+			player2_racket_y++;
+			if (player2_racket_y+RACKET_SIZE >= FIELD_HEIGHT) {
+				player2_racket_y-=RACKET_SIZE-1;
+			}
+		}
+	}
+}
+
+void 
+
+
 
 void draw_pole(int player1_racket_x, int player1_racket_y, int player2_racket_x,
             int player2_racket_y, int ball_cur_pos_x, int ball_cur_pos_y) {
@@ -88,7 +157,7 @@ void draw_pole(int player1_racket_x, int player1_racket_y, int player2_racket_x,
 
 			}
 		}
-
 	}
-
 }
+
+

fc541bb32667ca02bc54ac35fdc80b135824a7ae
unnamed <betacind@ga-j1.msk.21-school.ru>
2022-02-05 18:31:07 +0300

    Added a function to check the movement of the ball after a collision with the racket

--- src/pong.c
+++ src/pong.c
@@ -16,21 +16,44 @@
 # define WIN_SCORE 21
 # define PLAYER_1_MOVE 1
 # define PLAYER_2_MOVE 0
+# define PLAYER_MAX_SCORE 21
+# define INDENT 1
+# define BALL_MOVEMENT_LEFT -1
+# define BALL_MOVEMENT_RIGHT 1
+# define BALL_MOVEMENT_UP -1
+# define BALL_MOVEMENT_DOWN 1
+
+# define RACKET_P1_MOVEMENT_LEFT -1
+# define RACKET_P1_MOVEMENT_RIGHT 1
+# define RACKET_P1_MOVEMENT_UP -1
+# define RACKET_P1_MOVEMENT_DOWN 1
+
+# define RACKET_P2_MOVEMENT_LEFT -1
+# define RACKET_P2_MOVEMENT_RIGHT 1
+# define RACKET_P2_MOVEMENT_UP -1
+# define RACKET_P2_MOVEMENT_DOWN 1
+
 
 void game_end();
 void clear_screen();
 void draw_pole(int player1_racket_x, int player1_racket_y, int player2_racket_x,
             int player2_racket_y, int ball_cur_pos_x, int ball_cur_pos_y);
 
+// Ball definitions
 int ball_position();
+void ball_move (int x, int y);
+void checking_ball_racket_touching();
+
+// Players definitions
 int get_vertical_center_position();
 int player_turn(int ball_cur_pos_x);
 void get_move(int ball_cur_pos_x);
 
-
 //Structure of ball
 int ball_cur_pos_x;
 int ball_cur_pos_y;
+int ball_direction_v;
+int ball_direction_h;
 
 //Structure of player 1
 int player1_score ;
@@ -38,17 +61,21 @@ int player1_racket_x;
 int player1_racket_y;
 int player1_pressed_key;
 int player1_score;
+int player1_direction_v;
+int player1_direction_h;
 
-//Structure of player 1
+//Structure of player 2
 int player2_score ;
 int player2_racket_x;
 int player2_racket_y;
 int player2_pressed_key;
 int player2_score;
+int player2_direction_v;
+int player2_direction_h;
 
 
 int main() {
-	while (player1_score < 21 || player2_score < 21){
+	while (player1_score < PLAYER_MAX_SCORE || player2_score < PLAYER_MAX_SCORE){
 		// draw_pole (0, 0, 0, 0, 20, 20);
 		get_move(60);
 		// printf("%d\n", player2_racket_y);
@@ -69,13 +96,69 @@ void clear_screen() {
 }
 
 
+void ball_move (int x, int y) {
+	// The function checks for going beyond the field boundaries and 
+	// changes the coordinates of the ball according to the move
+	ball_cur_pos_x = x;
+	ball_cur_pos_y = y;
+
+	if (ball_direction_h == BALL_MOVEMENT_UP){
+		ball_cur_pos_y-=1;
+	}
+	if (ball_direction_h == BALL_MOVEMENT_DOWN){
+		ball_cur_pos_y+=1;
+	}
+	if (ball_direction_v == BALL_MOVEMENT_LEFT){
+		ball_cur_pos_x-=1;
+	}
+	if (ball_direction_v == BALL_MOVEMENT_RIGHT){
+		ball_cur_pos_x+=1;
+	}
+
+	if (ball_cur_pos_x < FIELD_WIDTH_START) {
+		ball_cur_pos_x = FIELD_WIDTH_START;
+	}
+	if (ball_cur_pos_x >= FIELD_WIDTH) {
+		ball_cur_pos_x = FIELD_WIDTH-1;
+	}
+	if (ball_cur_pos_y < FIELD_HEIGHT_START) {
+		ball_cur_pos_y = FIELD_HEIGHT_START;
+	}
+	if (ball_cur_pos_y >= FIELD_HEIGHT) {
+		ball_cur_pos_y = FIELD_HEIGHT-1;
+	}
+}
+
+
 void put_ball_position (int x, int y) {
 	// Put ball position
 	ball_cur_pos_x = x;
 	ball_cur_pos_y = y;
 }
 
+void checking_ball_racket_touching() {
+	// The function checks for touching rackets.
+	// When touched, changes the coordinates of the 
+	// ball to the opposite.
+	if(ball_cur_pos_y == player1_racket_y 
+			&& (ball_cur_pos_x == player1_racket_x 
+			|| player1_racket_x+RACKET_SIZE-1
+			|| player1_racket_x+RACKET_SIZE) ) {
+		ball_direction_v = BALL_MOVEMENT_LEFT;
+		ball_direction_h = player1_direction_h;
+	}
+
+	if(ball_cur_pos_y == player2_racket_y 
+			&& (ball_cur_pos_x == player2_racket_x 
+			|| player2_racket_x+RACKET_SIZE-1
+			|| player2_racket_x+RACKET_SIZE) ) {
+		ball_direction_v = BALL_MOVEMENT_LEFT;
+		ball_direction_h = player2_direction_h;
+	}
+}
+
 int get_vertical_center_position() {
+	// Function define center of field
 	return FIELD_WIDTH/2;
 }
 
@@ -99,12 +182,14 @@ void get_move(int ball_cur_pos_x){
 	if(player_turn(ball_cur_pos_x) == PLAYER_1_MOVE) {
 		if (key_pressed == KEY_P1_UP) {
 			player1_racket_y--;
+			player1_direction_v = RACKET_P1_MOVEMENT_UP;
 			if (player1_racket_y < 0) {
 				player1_racket_y=0;
 			}
 		}
 		if (key_pressed == KEY_P1_DOWN) {
 			player1_racket_y++;
+			player1_direction_v = RACKET_P1_MOVEMENT_DOWN;
 			if (player1_racket_y+RACKET_SIZE >= FIELD_HEIGHT) {
 				player1_racket_y-=RACKET_SIZE-1;
 			}
@@ -113,22 +198,21 @@ void get_move(int ball_cur_pos_x){
 	if(player_turn(ball_cur_pos_x) == PLAYER_2_MOVE) {
 		if (key_pressed == KEY_P2_UP) {
 			player2_racket_y--;
+			player2_direction_v = RACKET_P2_MOVEMENT_UP;
 			if (player2_racket_y < 0) {
 				player2_racket_y=0;
 			}
 		}
 		if (key_pressed == KEY_P2_DOWN) {
 			player2_racket_y++;
+			player2_direction_v = RACKET_P2_MOVEMENT_DOWN;
 			if (player2_racket_y+RACKET_SIZE >= FIELD_HEIGHT) {
 				player2_racket_y-=RACKET_SIZE-1;
 			}
 		}
 	}
+	checking_ball_racket_touching();
 }
-
-void 
-
-
 
 void draw_pole(int player1_racket_x, int player1_racket_y, int player2_racket_x,
             int player2_racket_y, int ball_cur_pos_x, int ball_cur_pos_y) {
